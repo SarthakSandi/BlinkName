46021627cc62b71d55f3095f88df25a8f3432136
SarthakSandi <141485239+SarthakSandi@users.noreply.github.com>
2023-08-19 14:40:48 +0530

    Add files via upload

---
+++ Blink.ino
@@ -0,0 +1,109 @@
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  //blinking first name: SAMARTH
+  //Blinking S in morse code
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);    
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(1000);       
+  //Blinking A in morse code 
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(300);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);             
+  digitalWrite(LED_BUILTIN, LOW); 
+  delay(1000);
+    //Blinking R in morse code  
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(300);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);    
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);         
+  digitalWrite(LED_BUILTIN, LOW); 
+  delay(1000);  
+  //Blinking T in morse code
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(300);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);             
+  digitalWrite(LED_BUILTIN, LOW); 
+  delay(1000);
+  //Blinking H in morse code 
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(1000);      
+  //Blinking A in morse code 
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(300);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);             
+  digitalWrite(LED_BUILTIN, LOW); 
+  delay(1000);
+    // Blinking K in morse code
+    digitalWrite(LED_BUILTIN, HIGH);
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(300);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  //name printed      
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(10000);    
+}
